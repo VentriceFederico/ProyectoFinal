346d5b06fef2a977826d0b2a6ddc3fdfa73437de
Federico Ventrice <ventricefederico@gmail.com>
2023-05-16 20:29:16 -0300

    Se esta por terminar el carrito!!!

--- JS/carrito.js
+++ JS/carrito.js
@@ -3,33 +3,74 @@ const productos_carrito = (JSON.parse(localStorage.getItem("productos_carrito"))
 const patchcords = (JSON.parse(localStorage.getItem("patchcords")) ?? []);
 
 
-function ver_patch_carrito(id) {
-    const contenedorProductos = document.getElementById("carrito");
-    const tarjetaProducto = document.createElement("div");
+function ver_patch_carrito(patchCarrito) {
+    const contenedorCarrito = document.getElementById("carrito");
 
+    const patch = patchcords.find((patch) => patch.id == patchCarrito.id);
 
-    const index = patchcords.findIndex((e) => e.id == id);
-
-    tarjetaProducto.className = "productos-tarjeta";
-    tarjetaProducto.id = "productos-tarjeta-" + patchcords[index].id;
-    tarjetaProducto.innerHTML = `
-                                <img src="./IMG/conectores.jpg" alt="Conectores">
-                                <h3>Patchcord de Fibra Optica</h3>
-                                <div class="productos-tarjeta-p">
-                                <p>Item ID: ${patchcords[index].id}</p>
-                                <p>Pathcord ${patchcords[index].tipo_fo} ${patchcords[index].conector_1} - ${patchcords[index].conector_2} ${patchcords[index].direccionamiento}</p>
-                                <p>Cantidad: ${productos_carrito[index].cant}</p>
-                                </div>
-                                `
-    
-    contenedorProductos.append(tarjetaProducto);
+    if (patch != undefined) {
+        const tdPatch = document.createElement("tr");
+        tdPatch.innerHTML = `
+            <td><img src="./IMG/conectores.jpg" alt=""></td>
+            <td><h3>Patchcord ${patch.tipo_fo} ${patch.direccionamiento} ${patch.conector_1} == ${patch.conector_2} de ${patch.mts} mts.</h3></td>
+            <td> $ ${patch.precio} USD</td>
+            <td>${patchCarrito.cant}</td>
+           `
+        contenedorCarrito.append(tdPatch);
+        console.log(patch);
+    }
+    else {
+        const index = productos_carrito.findIndex((producto) => producto.id == patchCarrito.id);
+        productos_carrito.splice(index, 1);
+        localStorage.setItem("productos_carrito", JSON.stringify(productos_carrito));
+    }
 }
 
 function imprimir_patch_carrito() {
-    productos_carrito.forEach(patch => {
-        ver_patch_carrito(patch.id);
-    });
+const carrito_visible = document.querySelector("#carrito");
+const botones_visible = document.querySelector(".botones");
+    if (productos_carrito.length == 0) {
+        
+        carrito_visible.classList.add("filter");
+        botones_visible.classList.add("filter");
+        Swal.fire({
+            title: 'No hay nada en el carrito',
+            icon: 'info',
+            timer: 2000,
+        })
+        setTimeout(() => {
+            location.href = "../index.html";
+        }, 2000)
+    }
+    else {
+        carrito_visible.classList.remove("filter");
+        botones_visible.classList.remove("filter");
+        productos_carrito.forEach(patch => {
+            ver_patch_carrito(patch);
+        });
+    }
+
+
 
 }
 
+function borrar_patch_carrito() {
+    const btnBorrarCarrito = document.getElementById("btnBorrarCarrito");
+
+    btnBorrarCarrito.addEventListener("click", () => {
+        localStorage.setItem("productos_carrito", JSON.stringify([]));
+        Toastify({
+            text: "Se elimino el Carrito",
+            duration: 3000,
+            style: {
+                background: "linear-gradient(to right, #FE0C0D, #000000)",
+            },
+        }).showToast()
+        setTimeout(() => {
+            location.href = "../index.html";
+        }, 1500);
+    })
+}
+
 imprimir_patch_carrito();
+borrar_patch_carrito();
